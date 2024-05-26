53775ebe1150f0d03b1a12bcdf64e11df80cbc80
saraawaad2 <sara.schoo22.25@gmail.com>
2024-05-26 09:37:57 +0300

    my first version

--- app/src/main/java/com/homework/gethub/MainActivity.java
+++ app/src/main/java/com/homework/gethub/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 int x;
+boolean f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
